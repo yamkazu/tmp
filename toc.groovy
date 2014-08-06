28d7a5815e10ddc2a118c047930b15ed5e7c44cd
Kazuki YAMAMOTO <yam.kazuki@gmail.com>
2014-08-06 10:39:03 +0900

    init

---
+++ toc.groovy
@@ -0,0 +1,39 @@
+// example:
+//
+// "introduction.html"(title: "Introduction") {
+//     "introduction/whatis.html"(title: "What is Gaiden?")
+//     "introduction/install.html"(title: "Install")
+// }
+// "quickstart/quickstart.html"(title: "Quick Start") {
+//     "quickstart/addingcontent.html"(title: "Adding content")
+// }
+//
+//"index.md"(title: "はじめに")
+//"/index.md"(title: "はじめに")
+//"index"(title: "はじめに")
+//"/index"(title: "はじめに")
+//"index.html"(title: "はじめに")
+//
+//"index.md#hash"(title: "はじめに")
+//"/index.md#hash"(title: "はじめに")
+//"index#hash"(title: "はじめに")
+//"/index#hash"(title: "はじめに")
+//"index.html#hash"(title: "はじめに")
+//
+//"sub/sub.md"(title: "サブページ")
+//"/sub/sub.md"(title: "サブページ")
+//"sub/sub"(title: "サブページ")
+//"/sub/sub"(title: "サブページ")
+//"sub/sub.html"(title: "サブページ")
+//"/sub/sub.html"(title: "サブページ")
+//
+//"sub/sub.md#hash"(title: "サブページ")
+//"/sub/sub.md#hash"(title: "サブページ")
+//"sub/sub#hash"(title: "サブページ")
+//"/sub/sub#hash"(title: "サブページ")
+//"sub/sub.html#hash"(title: "サブページ")
+//"/sub/sub.html#hash"(title: "サブページ")
+
+"index.md"(title: "はじめに")
+"sub/sub.md"(title: "サブページ")
+
